e395e2d1c498b897ca8a22361d65ca2919aca5e0
lmalfonzo <lmalfonzo@dons.usfca.edu>
2020-10-09 19:25:08 -0700

    highlight switcher :crab:

---
+++ GrowHome/Assets/Scripts/Materials/MaterialChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+
+    public Material[] materials;
+
+    public int size;
+
+    Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = 0;
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = materials[size];
+    }
+
+    void OnValidate() //TODO find out how to trigger this on boxcast (and untrigger this on boxcast out)
+    {
+        rend.sharedMaterial = materials[size];
+    }
+
+}
+    
