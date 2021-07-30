f073c5157d5835988bebcac3ac79f75a1b7b2c6c
lmalfonzo <lmalfonzo@dons.usfca.edu>
2021-07-30 01:41:17 -0700

    welcome back graph :crab:

---
+++ GrowHome/Assets/Scripts/Graphs/Graph.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+
+    [SerializeField, Range(10,100)]
+    int resolution = 10;
+
+    void Awake()
+    {
+        float step = resolution / 2f;
+        Vector3 position = Vector3.zero;
+        var scale = Vector3.one / step; 
+        for (int i = 0; i < resolution; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            //point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
+            position.x = ((i + 0.5f) / step - 1f);
+            position.y = position.x * position.x; //graph input
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
+        
+    }
+}
